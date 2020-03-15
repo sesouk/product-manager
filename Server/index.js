f2788681f7a990aa9c163dc45966f48290827f33
Kevin Tran <sesouk@gmail.com>
2020-03-14 22:36:40 -0600

    server running and connected to database

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const ctrl = require('./controller')
+
+const app = express()
+app.use(express.json())
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+  app.set('db', dbInstance)
+}).catch(err => console.log('ERROR', err))
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server port is over ${SERVER_PORT}!`)
+})
---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Product from '../Product/Product'
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <>
+      Dashboard
+      <Product/>
+      </>
+    );
+  }
+}
